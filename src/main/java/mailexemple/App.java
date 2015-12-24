a7a1218e640107fc545a8d20f711fd8c56a7fefb
Mounir KERKAR <kerkarmounir@yahoo.fr>
2015-12-24 22:19:14 +0100

    init

---
+++ src/main/java/mailexemple/App.java
@@ -0,0 +1,20 @@
+package mailexemple;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	private static ApplicationContext context;
+
+	public static void main(String[] args) {
+		context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		SendMailExampl mm = (SendMailExampl) context.getBean("sendMailExampl");
+        mm.sendMail("sender@gmail.com",
+    		   "receiver@gmail.com",
+    		   "Javacodegeeks email test", 
+    		   "Testing.. \n Hello Spring Email Sender");
+	}
+}
